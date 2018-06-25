ba649f697726a2d666636aa208a57620fb37a06d
ayu-mushi <ayu.mushi2015@gmail.com>
2018-06-25 20:17:16 +0900

    Update dotfilesLink.sh

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -3,12 +3,15 @@ ln -sf ~/Dropbox/dotfiles/.vimrc ~/.vimrc
 ln -sf ~/Dropbox/dotfiles/.zshrc ~/.zshrc
 ln -sf ~/Dropbox/dotfiles/keymap ~/.w3m/keymap
 ln -sf ~/Dropbox/dotfiles/bookmark.html ~/.w3m/bookmark.html
-ln -sf ~/Dropbox/dotfiles/.zsh_history ~/.zsh_history
 ln -sf ~/Dropbox/dotfiles/urls ~/.newsbeuter/urls
 ln -sf ~/Dropbox/dotfiles/urimethodmap ~/.w3m/urimethodmap
 ln -sf ~/Dropbox/dotfiles/newsbeutconf ~/.newsbeuter/config
-ln -sf ~/Dropbox/dotfiles/.zsh_history* ~
 ln -sf ~/Dropbox/dotfiles/.emacs ~
 ln -sf ~/Dropbox/dotfiles/.xsessionrc ~
+ln -sf ~/Dropbox/dotfiles/.Xdefaults ~
+ln -sf ~/Dropbox/dotfiles/mailcap ~/.w3m/
+ln -sf ~/Dropbox/dotfiles/.xmobarrc ~/.xmobarrc
+ln -sf ~/Dropbox/dotfiles/.stalonetrayrc ~/.stalonetrayrc
+mkdir -p ~/.xmonad/ 2>/dev/null
 ln -sf ~/Dropbox/dotfiles/xmonad.hs ~/.xmonad/xmonad.hs
 source ~/.zshrc
