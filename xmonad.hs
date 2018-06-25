c2dd3b350b1bbeade01ddc94bf7a5dc2005d69a8
ayu-mushi <ayu.mushi2015@gmail.com>
2018-06-25 20:16:13 +0900

    Update .xmonad.

--- xmonad.hs
+++ xmonad.hs
@@ -10,14 +10,32 @@
 import XMonad
 import Data.Monoid
 import System.Exit
+import System.Cmd(system)
 
 import System.IO
+import Control.Concurrent (forkIO, MVar, readMVar, newMVar, modifyMVar, modifyMVar_, putMVar, threadDelay)
 import XMonad.Hooks.DynamicLog hiding (def)
 import XMonad.Hooks.ManageDocks
+import XMonad.Hooks.EwmhDesktops (ewmh, ewmhDesktopsStartup, ewmhDesktopsLogHook, ewmhDesktopsEventHook)
 import XMonad.Util.Run (spawnPipe)
-
+import XMonad.Util.Cursor (xC_pirate, setDefaultCursor, xC_arrow)
+import XMonad.Config.Xfce (xfceConfig)
+import XMonad.Config.Gnome (gnomeConfig)
 import qualified XMonad.StackSet as W
+import XMonad.Actions.SpawnOn (spawnOn)
+--import ViewDoc (toggleSaveState, launchDocuments, colorSaved)
+
+import Control.Monad (void, when)
 import qualified Data.Map        as M
+
+import XMonad.Util.EZConfig (additionalKeysP, additionalKeys)
+import Graphics.X11.ExtraTypes.XF86 (xF86XK_AudioMute, xF86XK_MonBrightnessDown, xF86XK_MonBrightnessUp)
+
+
+import XMonad.Hooks.UrgencyHook (UrgencyHook(urgencyHook), withUrgencyHook)
+import XMonad.Util.NamedWindows
+import XMonad.Util.Run
+
 
 -- The preferred terminal program, which is used in a binding below and by
 -- certain contrib modules.
@@ -50,9 +68,9 @@ myModMask       = mod4Mask
 --
 -- A tagging example:
 --
--- > workspaces = ["web", "irc", "code" ] ++ map show [4..9]
+myWorkspaces = ["w3m", "browser", "code", "doc", "music" ] ++ map show [6..8] ++ ["trash"]
 --
-myWorkspaces    = ["1","2","3","4","5","6","7","8","9"]
+--myWorkspaces    = ["1","2","3","4","5","6","7","8","9"]
 
 -- Border colors for unfocused and focused windows, respectively.
 --
@@ -62,14 +80,35 @@ myFocusedBorderColor = "#ff0000"
 ------------------------------------------------------------------------
 -- Key bindings. Add, modify or remove key bindings here.
 --
-myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
+myKeys isInConc conf@(XConfig {XMonad.modMask = modm}) = M.fromList $ let windows_frzabl = myWindows isInConc in
 
     -- launch a terminal
     [ ((modm .|. shiftMask, xK_Return), spawn $ XMonad.terminal conf)
-
+    , ((0, xF86XK_MonBrightnessUp), spawn "light -A 1") -- #define XF86XK_MonBrightnessUp   0x1008FF02  /* Monitor/panel brightness */ -- https://github.com/haikarainen/light
+    , ((0, xF86XK_MonBrightnessDown), spawn "light -U 1") -- #define XF86XK_MonBrightnessDown 0x1008FF03  /* Monitor/panel brightness */
+    , ((0, xF86XK_AudioMute), spawn "amixer -q -D pulse sset Master toggle") -- #define XF86XK_AudioMute	0x1008FF12   /* Mute sound from the system */
+    , ((modm, xK_Print), spawn "gnome-screenshot --area") -- xK_Print does not work.
+    --, ((modm, 0xFF61), spawn "gnome-screenshot --area")
     , ((modm, xK_d), spawn "emacs")
     , ((modm, xK_e), spawn "emacs")
-    , ((modm, xK_f), spawn "firefox")
+    --, ((modm, xK_s), toggleSaveState)
+    , ((modm, xK_s), io $ do
+        i <- readMVar isInConc
+        when (not i) $ do
+          modifyMVar_ isInConc $ \x -> return True
+          forkIO $ do threadDelay (60 * 10^6)
+                      modifyMVar_ isInConc $ \x -> return False
+          return ()
+      )
+    --, ((modm .|. shiftMask, xK_s), launchDocuments)
+    , ((modm, xK_f), spawn "x-www-browser")
+    , ((modm, xK_a), spawn "krita")
+    , ((modm, xK_v), spawn "gvim")
+    , ((modm .|. shiftMask, xK_a), spawn "atom")
+    -- logout
+    , ((modm .|. shiftMask, xK_q), spawn "gnome-session-save --logout-dialog -gui")
+    -- change window manager from XMonad to xfw4
+    , ((modm .|. controlMask, xK_q), (io $ forkIO $ void $ system ("sleep 2s && xfwm4 &")) >> (io $ exitWith ExitSuccess))
     , ((modm .|. shiftMask, xK_m), spawn "mendeleydesktop")
     -- launch dmenu
     , ((modm,               xK_p     ), spawn "dmenu_run")
@@ -88,6 +127,7 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
 
     -- Resize viewed windows to the correct size
     , ((modm,               xK_n     ), refresh)
+    , ((modm .|. shiftMask, xK_t), spawn "nautilus ~") -- or Thunar
 
     -- Move focus to the next window
     , ((modm,               xK_Tab   ), windows W.focusDown)
@@ -129,10 +169,8 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     -- Use this binding with avoidStruts from Hooks.ManageDocks.
     -- See also the statusBar function from Hooks.DynamicLog.
     --
-    -- , ((modm              , xK_b     ), sendMessage ToggleStruts)
+    , ((modm              , xK_b     ), sendMessage ToggleStruts)
 
-    -- Quit xmonad
-    , ((modm .|. shiftMask, xK_q     ), io (exitWith ExitSuccess))
 
     -- Restart xmonad
     , ((modm              , xK_q     ), spawn "xmonad --recompile; xmonad --restart")
@@ -146,16 +184,17 @@ myKeys conf@(XConfig {XMonad.modMask = modm}) = M.fromList $
     -- mod-[1..9], Switch to workspace N
     -- mod-shift-[1..9], Move client to workspace N
     --
-    [((m .|. modm, k), windows $ f i)
+    [((m .|. modm, k), windows_frzabl $ f i)
         | (i, k) <- zip (XMonad.workspaces conf) [xK_1 .. xK_9]
         , (f, m) <- [(W.greedyView, 0), (W.shift, shiftMask)]]
     ++
+    -- shiftの場合はfrzableじゃなくていいんだけど…
 
     --
     -- mod-{w,e,r}, Switch to physical/Xinerama screens 1, 2, or 3
     -- mod-shift-{w,e,r}, Move client to screen 1, 2, or 3
     --
-    [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows . f))
+    [((m .|. modm, key), screenWorkspace sc >>= flip whenJust (windows_frzabl . f))
         | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
         , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
 
@@ -173,8 +212,9 @@ myMouseBindings (XConfig {XMonad.modMask = modm}) = M.fromList $
     , ((modm, button2), (\w -> focus w >> windows W.shiftMaster))
 
     -- mod-button3, Set the window to floating mode and resize by dragging
-    , ((modm, button3), (\w -> focus w >> mouseResizeWindow w
-                                       >> windows W.shiftMaster))
+    -- DISABLED: because it is annoying to me...
+    {-, ((modm, button3), (\w -> focus w >> mouseResizeWindow w
+                                       >> windows W.shiftMaster))-}
 
     -- you may also bind events to the mouse scroll wheel (button4 and button5)
     ]
@@ -224,7 +264,9 @@ myManageHook = composeAll
     --, className =? "Gimp"           --> doFloat
     , (role =? "gimp-toolbox" <||> role =? "gimp-image-window") --> (ask >>= doF . W.sink)
     , resource  =? "desktop_window" --> doIgnore
-    , resource  =? "kdesktop"       --> doIgnore ]
+    , resource  =? "kdesktop"       --> doIgnore
+    , resource  =? "stalonetray"       --> doIgnore
+    ]
   where role = stringProperty "WM_WINDOW_ROLE"
 
 ------------------------------------------------------------------------
@@ -236,7 +278,7 @@ myManageHook = composeAll
 -- return (All True) if the default handler is to be run afterwards. To
 -- combine event hooks use mappend or mconcat from Data.Monoid.
 --
-myEventHook = mempty
+--myEventHook = mempty
 
 ------------------------------------------------------------------------
 -- Status bars and logging
@@ -254,7 +296,10 @@ myEventHook = mempty
 -- per-workspace layout choices.
 --
 -- By default, do nothing.
-myStartupHook = return ()
+myStartupHook :: X ()
+myStartupHook = do
+  setDefaultCursor xC_arrow -- デフォルトのカーソルを普通の矢印にする
+  return ()
 
 ------------------------------------------------------------------------
 -- Now run xmonad with all the defaults we set up.
@@ -263,7 +308,12 @@ myStartupHook = return ()
 --
 main = do
   myStatusBar <- spawnPipe "xmobar"
-  xmonad $ defaults $ dynamicLogWithPP $ xmobarPP { ppOutput = hPutStrLn myStatusBar}
+  isInConc <- newMVar False
+  xmonad $
+    withUrgencyHook LibNotifyUrgencyHook $
+      defaults isInConc $
+        dynamicLogWithPP $
+          xmobarPP { ppOutput = hPutStrLn myStatusBar}
 
 -- A structure containing your configuration settings, overriding
 -- fields in the default config. Any you don't override, will
@@ -273,8 +323,7 @@ main = do
 --
 
 -- def = defaultConfig
-
-defaults mLH = defaultConfig {
+defaults isInConc mLH = gnomeConfig {
       -- simple stuff
         terminal           = myTerminal,
         focusFollowsMouse  = myFocusFollowsMouse,
@@ -286,16 +335,26 @@ defaults mLH = defaultConfig {
         focusedBorderColor = myFocusedBorderColor,
 
       -- key bindings
-        keys               = myKeys,
+        keys               = myKeys isInConc,
         mouseBindings      = myMouseBindings,
 
       -- hooks, layouts
         layoutHook         = avoidStruts myLayout,
         manageHook         = myManageHook,
-        handleEventHook    = myEventHook,
-        logHook            = mLH,
-        startupHook        = myStartupHook
+        --handleEventHook    = ewmhDesktopsEventHook,
+        logHook            = mLH, -- >> colorSaved,
+        startupHook        = myStartupHook -- >> ewmhDesktopsStartup
     }
+    `additionalKeysP`
+    [
+    -- screen shot.
+    ("<PrtSc>", spawn "gnome-screenshot --area")
+    , ("<Pause>", spawn "gnome-screenshot --area")
+    -- adjust backlight
+    --, ("<F6>", spawn "light -U 1") -- https://github.com/haikarainen/light
+    --, ("<F7>", spawn "light -A 1")
+    --, ("<F1>", spawn "amixer -q -D pulse sset Master toggle")
+    ]
 
 -- | Finally, a copy of the default bindings in simple textual tabular format.
 help :: String
@@ -347,3 +406,20 @@ help = unlines ["The default modifier key is 'alt'. Default keybindings:",
     "mod-button1  Set the window to floating mode and move by dragging",
     "mod-button2  Raise the window to the top of the stack",
     "mod-button3  Set the window to floating mode and resize by dragging"]
+
+-- bindable version of `windows`
+-- MVar
+myWindows :: MVar Bool -> (WindowSet -> WindowSet) -> X ()
+myWindows v f = do
+  isInConc <- io $ readMVar v
+  when (not isInConc) $ windows f
+
+
+data LibNotifyUrgencyHook = LibNotifyUrgencyHook deriving (Read, Show)
+
+instance UrgencyHook LibNotifyUrgencyHook where
+  urgencyHook LibNotifyUrgencyHook w = do
+    name <- getName w
+    Just idx <- fmap (W.findTag w) $ gets windowset
+
+    safeSpawn "notify-send" [show name, "workspace " ++ idx]
